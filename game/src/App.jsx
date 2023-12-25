0e06ab0e006104c5a11fc9fdbfa600dc9274f5b8
SarahQW <108081862+SarahQW@users.noreply.github.com>
2023-12-26 02:39:11 +0300

    Add responsive styling

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import StagesGame from "./component/StagesGame";
+import ExplainGame from "./component/ExplainGame";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/StagesGame" element={<StagesGame />} />
+        <Route path="/ExplainGame" element={<ExplainGame />} />
       </Routes>
     </>
   );
--- game/src/component/ExplainGame.jsx
+++ game/src/component/ExplainGame.jsx
@@ -2,10 +2,29 @@ import React from "react";
 
 function ExplainGame() {
   return (
-    <div>
-      {" "}
-      <div>ExplainGame</div> <div>ExplainGame</div> <div>ExplainGame</div>
-    </div>
+    <>
+      {/* start section */}
+      <section className="min-h-screen w-scree">
+        {/* image Background */}
+        <div
+          className="relative overflow-hidden bg-cover bg-no-repeat p-12 text-center h-screen max-sm:overflow-auto max-md:overflow-auto"
+          style={{
+            backgroundImage: `url()`,
+            backgroundSize: `cover`,
+            backgroundPosition: `center`,
+          }}
+        >
+          <div
+            className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed max-sm:overflow-auto max-md:overflow-auto"
+            style={{ backgroundColor: `rgba(0, 0, 0, 0.3)` }}
+          >
+            {/* conentent */}
+          </div>
+        </div>
+
+        {/* End section */}
+      </section>
+    </>
   );
 }
 
