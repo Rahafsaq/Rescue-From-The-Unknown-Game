1438036cfeefe26efbdfb7f65afa4ba631dee62f
Rahafsaq <rahafalqahtani11@hotmail.com>
2023-12-25 22:38:54 +0300

    add characters page

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,13 +1,11 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-    <>
-  <h1 className='text-red-900 text-center'>Hellow EveryOne</h1>
-    </>
-  )
+	return (
+		<>
+			<h1 className='text-red-900 text-center'>Hellow EveryOne</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
