a3268f7fa15f389d140330fca0f24fbab8b123ac
Rahafsaq <rahafalqahtani11@hotmail.com>
2023-12-25 22:53:42 +0300

    edit app.jsx

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,29 +1,5 @@
-
-import './App.css';
-
 function App() {
-	return (
-		<>
-			<h1 className='text-red-900 text-center'>Hellow EveryOne</h1>
-		</>
-	);
-
-
-import './App.css'
-import { Route , Routes } from 'react-router-dom'
-import Homepage from './Pages/Homepage'
-import Errorpage from './Pages/Errorpage'
-function App() {
-
-  return (
-    <>
-  <Routes>
-    <Route path='/' element={<Homepage/>}></Route>
-    <Route path='/*' element={<Errorpage/>}></Route>
-    
-  </Routes>
-    </>
-  )
+	return <></>;
 }
 
 export default App;
