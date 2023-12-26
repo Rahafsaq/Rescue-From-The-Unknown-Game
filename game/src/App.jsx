e867745ac85d5164fd712843788dd3c199b2f79c
SarahQW <108081862+SarahQW@users.noreply.github.com>
2023-12-26 21:22:40 +0300

    changes

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,16 +1,5 @@
-import { Routes } from "react-router-dom";
-import ExplainGame from "./component/ExplainGame";
-import StagesGame from "./component/StagesGame";
-
 function App() {
-  return (
-    <>
-      <ExplainGame />
-      <div>
-        <StagesGame />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
