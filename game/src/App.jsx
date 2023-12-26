acf06f5cb7a10083512ba38f250957879a161778
SarahQW <108081862+SarahQW@users.noreply.github.com>
2023-12-26 21:21:25 +0300

    Add Component

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,5 +1,16 @@
+import { Routes } from "react-router-dom";
+import ExplainGame from "./component/ExplainGame";
+import StagesGame from "./component/StagesGame";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ExplainGame />
+      <div>
+        <StagesGame />
+      </div>
+    </>
+  );
 }
 
 export default App;
